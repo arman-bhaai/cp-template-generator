9aeacb962792d767e5f3f35ce79da4c86cd7328a
arman-bhaai <arman.bhaai@gmail.com>
2022-10-19 11:06:10 +0600

    updated template

--- template1.cpp
+++ template1.cpp
@@ -1,46 +1,67 @@
 /*______________________________________________________________________
 ||--------------------------------------------------------------------||
 ||                                                                    ||
+|| "I bear witness that there is no god but Allah and I bear witness  ||
+||    that Muhammad (peace be upon him) is the Messenger of Allah"    ||
+||                                                                    ||
 || *** In the name of Allah, the Most Gracious, the Most Merciful.*** ||
 ||                                                                    ||
-||--------------------------------------------------------------------||
-||--------------------------------------------------------------------||
+========================================================================
+========================================================================
+||                                                                    ||
 || --> Author     :     Abu Bakar Siddique Arman (#arman_bhaai)       ||
 || --> Email      :     arman.bhaai@gmail.com                         ||
 || --> Portfolio  :     arman-bhaai.github.io                         ||
 || --> LinkedIn   :     linkedin.com/in/abubakar-arman                ||
-|| --> Facebook   :     fb.me/arman.bhaai                             ||
 || --> GitHub     :     github.com/arman-bhaai                        ||
+|| --> Facebook   :     fb.me/arman.bhaai                             ||
 || --> StopStalk  :     stopstalk.com/user/profile/arman_bhaai        ||
+|| --> Clist      :     clist.by/coder/arman_bhaai                    ||
 || --> CodeForces :     codeforces.com/profile/arman_bhaai            ||
 || --> CodeChef   :     codechef.com/users/arman_bhaai                ||
+|| --> AtCoder    :     atcoder.jp/users/arman_bhaai                  ||
 || --> HackerRank :     hackerrank.com/arman_bhaai                    ||
-||--------------------------------------------------------------------||
-||--------------------------------------------------------------------*/
+|| --> LeetCode   :     leetcode.com/arman_bhaai                      ||
+||____________________________________________________________________||
+----------------------------------------------------------------------*/
 
 // I believe in OpenSource. So, any of my code snippets are Copyright-Free.
 // <3 Happy Coding <3
 
-// Problem : 
+// Contest ID   :: 
+// Problem Name :: 
+// Problem URL  :: 
+
+/*******************************************************************************
+////////////////////////////////////////////////////////////////////////////////
+*******************************************************************************/
 
 
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
+typedef string str;
 typedef vector<int> vi;
-typedef vector<long long> vii;
+typedef vector<ll> vii;
 typedef vector<vector<ll>> vvi;
+typedef map<int,int> mi;
+typedef map<ll,ll> mii;
 
-#define couts(i) cout<<i<<" "
-#define coutn(i) cout<<i<<'\n'
+// shortcuts
+#define endl '\n'
+#define F first
+#define S second
+#define getv(v) for(auto &k:v) cin>>k;
+#define all(v) v.begin(),v.end()
+// debugging
+#define couts(i) cout<<i<<" ";
+#define coutn(i) cout<<i<<'\n';
 #define showv(v) cout<<endl;for(auto &k:v) cout<<k<<", ";cout<<endl;
+#define showm(m) cout<<endl;for(auto &k:m) cout<<k.F<<" -> "<<k.S<<endl;cout<<endl;
 
-void optimize(){
-	// fastIO
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-}
-
+void optimize();
+// void solve();
 
 
 void solve(){
@@ -55,6 +76,14 @@ int main(){
 		// cout<<"Case "<<_<<": ";
 		solve(); cout<<endl;
 	}
+	return 0;
+}
+
+
+void optimize(){
+	// fastIO
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 }
 
 /* Alternatives
--- template2.cpp
+++ template2.cpp
@@ -1,55 +1,100 @@
 /*______________________________________________________________________
 ||--------------------------------------------------------------------||
 ||                                                                    ||
+|| "I bear witness that there is no god but Allah and I bear witness  ||
+||    that Muhammad (peace be upon him) is the Messenger of Allah"    ||
+||                                                                    ||
 || *** In the name of Allah, the Most Gracious, the Most Merciful.*** ||
 ||                                                                    ||
-||--------------------------------------------------------------------||
-||--------------------------------------------------------------------||
+========================================================================
+========================================================================
+||                                                                    ||
 || --> Author     :     Abu Bakar Siddique Arman (#arman_bhaai)       ||
 || --> Email      :     arman.bhaai@gmail.com                         ||
 || --> Portfolio  :     arman-bhaai.github.io                         ||
 || --> LinkedIn   :     linkedin.com/in/abubakar-arman                ||
-|| --> Facebook   :     fb.me/arman.bhaai                             ||
 || --> GitHub     :     github.com/arman-bhaai                        ||
+|| --> Facebook   :     fb.me/arman.bhaai                             ||
 || --> StopStalk  :     stopstalk.com/user/profile/arman_bhaai        ||
+|| --> Clist      :     clist.by/coder/arman_bhaai                    ||
 || --> CodeForces :     codeforces.com/profile/arman_bhaai            ||
 || --> CodeChef   :     codechef.com/users/arman_bhaai                ||
+|| --> AtCoder    :     atcoder.jp/users/arman_bhaai                  ||
 || --> HackerRank :     hackerrank.com/arman_bhaai                    ||
-||--------------------------------------------------------------------||
-||--------------------------------------------------------------------*/
+|| --> LeetCode   :     leetcode.com/arman_bhaai                      ||
+||____________________________________________________________________||
+----------------------------------------------------------------------*/
 
 // I believe in OpenSource. So, any of my code snippets are Copyright-Free.
 // <3 Happy Coding <3
 
+// Contest ID   :: 
+// Problem Name :: 
+// Problem URL  :: 
 
-// Problem : 
+/*******************************************************************************
+////////////////////////////////////////////////////////////////////////////////
+*******************************************************************************/
+
 
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
+typedef string str;
 typedef vector<int> vi;
-typedef vector<long long> vii;
+typedef vector<ll> vii;
 typedef vector<vector<ll>> vvi;
+typedef map<int,int> mi;
+typedef map<ll,ll> mii;
 
-#define mpb push_back
+// shortcuts
 #define endl '\n'
+#define F first
+#define S second
+#define getv(v) for(auto &k:v) cin>>k;
+#define all(v) v.begin(),v.end()
+#define pb push_back
 #define rep(i,n) for (int i=0; i<n; i++)
 #define repr(i,n) for (int i=n, i>0; i--)
 #define rep2(i,n,k) for (int i=k; i<n; i++)
 #define repr2(i,n,k) for (int i=n, i>k; i--)
-#define watch(x) cout << (#x) << " is " << (x) << endl
-#define couts(i) cout<<i<<" "
-#define coutn(i) cout<<i<<'\n'
 #define sortva(v) sort(v.begin(), v.end())
 #define out(flag) cout << (flag ? "YES" : "NO");
-#define ff first
-#define ss second
+// debugging
+#define couts(i) cout<<i<<" ";
+#define coutn(i) cout<<i<<'\n';
+#define showv(v) cout<<endl;for(auto &k:v) cout<<k<<", ";cout<<endl;
+#define showm(m) cout<<endl;for(auto &k:m) cout<<k.F<<" -> "<<k.S<<endl;cout<<endl;
+#define watch(x) cout << (#x) << " is " << (x) << endl;
+
+void optimize();
+bool isVowel(char c);
+bool isPrime(ll n);
+// void solve();
+
+
+void solve(){
+	
+}
+
+int main(){
+	optimize();
+	int __=1;
+	cin>>__;
+	for(int _=1; _<=__; _++){
+		// cout<<"Case "<<_<<": ";
+		solve(); cout<<endl;
+	}
+	return 0;
+}
+
 
 void optimize(){
 	// fastIO
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 }
 
@@ -66,20 +111,4 @@ bool isPrime(ll n){
 		if(n%i==0 || n%(i+2)==0) return false;
 	}
 	return true;
-}
-
-
-
-void solve(){
-	
-}
-
-int main(){
-	optimize();
-	int __=1;
-	// cin>>__;
-	for(int _=1; _<=__; _++){
-		// cout<<"Case "<<_<<": ";
-		solve(); cout<<endl;
-	}
 }
